68e2645f2f6d76ab606ea5357805b86bfb25b5df
Ramin Haghi <raminhaghi33@gmail.com>
2024-11-05 23:28:02 +0330

    coding and styles and script for recipes section

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,17 @@
 const navToggleBtn = document.querySelector('.menu-toggle');
 const menu = document.querySelector('.menu');
 const cover = document.querySelector('.cover');
-
+const likeRecipeWrapper = document.querySelectorAll('.recipes-like');
+const likeRecipes = document.querySelectorAll('.recipes-like__icon'); // Select all like icons
 
 navToggleBtn.addEventListener('click', function () {
     this.classList.toggle('menu-toggle--open')
     menu.classList.toggle('menu--open')
     cover.classList.toggle('cover--show')
-})
+});
+
+likeRecipeWrapper.forEach(function (likeRecipe) {
+    likeRecipe.addEventListener('click', function () {
+        this.classList.toggle('recipes-like__icon--liked'); // Toggle the class only on the clicked element
+    });
+});
